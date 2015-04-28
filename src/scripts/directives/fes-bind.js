a0129562970ac4e767f01a2331454123c58eda57
bernardmoes <bernardmoes@live.nl>
2015-04-28 15:02:21 +0200

    fes-bind opnieuw geschreven

--- src/scripts/directives/fes-bind.js
+++ src/scripts/directives/fes-bind.js
@@ -2,8 +2,30 @@ honeydew.directive('fesBind', ['FesInterface', function(FesInterface)
 {
     return {
         restrict: 'A',
-        link: function(scope, element, attrs) {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            var variable = attrs.ngModel;
+            var fesvariable = FesInterface.getVariable(variable);
 
+            scope[variable] = fesvariable.getValue(1);
+            scope.$watch(variable, function(newvalue, oldvalue) {
+                if(newvalue != oldvalue)
+                {
+                    var dependencies = fesvariable.setValue(1, newvalue);
+                    var length = dependencies.length;
+                    for(var i=0; i<length; i++)
+                    {
+                        if(angular.isDefined(scope[dependencies[i]]))
+                        {
+                            scope[dependencies[i]] = FesInterface.getVariable(dependencies[i]).getValue(1);
+                            console.log("dependencie " + dependencies[i] + " set!");
+                        }
+                    }
+                }
+                /*
+
+                */
+            });
         }
     }
 }])
