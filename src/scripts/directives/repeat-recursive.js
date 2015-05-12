9fdffae04b9b51afa6b9337606d5518e1d4fe31b
Jan-Paul Kleemans <jpkleemans@gmail.com>
2015-05-12 15:43:30 +0200

    Repeat recursive directive review

---
+++ src/scripts/directives/repeat-recursive.js
@@ -0,0 +1,50 @@
+honeydew.directive('repeatRecursive', [
+    'VariableRepository',
+    '$compile',
+    '$templateCache',
+    function (Variables, $compile, $templateCache) {
+        /**
+         *
+         * @param element
+         * @param attrs
+         */
+        var compile = function (element, attrs) {
+            $templateCache.put(attrs.repeatRecursive, element[0].innerHTML);
+
+            var splittedRepeat = attrs.ngRepeat.split(' in ');
+            var variable = splittedRepeat[1];
+
+            var children = Variables.findByKey(variable).getChildren();
+
+            var result = getChildrenRecursive(children);
+
+            attrs.ngRepeat = splittedRepeat[0] + ' in ' + JSON.stringify(result);
+        };
+
+        /**
+         *
+         * @param child
+         * @returns {Array}
+         */
+        var getChildrenRecursive = function (children) {
+            var result = [];
+
+            var i = 0;
+            var length = children.length;
+            while (i < length) {
+                result.push({
+                    key: children[i].key,
+                    children: getChildrenRecursive(children[i].getChildren())
+                });
+                i++
+            }
+
+            return result;
+        };
+
+        return {
+            restrict: 'A',
+            priority: 1002,
+            compile: compile
+        }
+    }]);
