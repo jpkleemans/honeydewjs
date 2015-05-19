35add29981bba203c7ee8ece022149a3542befc7
bernardmoes <bernardmoes@live.nl>
2015-05-19 10:50:36 +0200

    updated repeat recursive.

--- src/scripts/directives/repeat-recursive.js
+++ src/scripts/directives/repeat-recursive.js
@@ -16,33 +16,9 @@ honeydew.directive('repeatRecursive', [
 
             var children = Variables.findByKey(variable).getChildren();
 
-            var result = getChildrenRecursive(children);
+            attrs.ngRepeat = splittedRepeat[0] + ' in ' + JSON.stringify(children);
 
-            attrs.ngRepeat = splittedRepeat[0] + ' in ' + JSON.stringify(result);
-        };
 
-        /**
-         * Get recursive array of children
-         *
-         * @param child
-         * @returns {Array}
-         */
-        var getChildrenRecursive = function (children) {
-            var result = [];
-
-            var i = 0;
-            var length = children.length;
-            while (i < length) {
-                var childs = children[i].getChildren();
-                result.push({
-                    key: children[i].key,
-                    children: getChildrenRecursive(childs),
-                    hasChildren: childs.length > 0
-                });
-                i++;
-            }
-
-            return result;
         };
 
         return {
