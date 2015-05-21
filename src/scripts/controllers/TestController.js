fd9bf9b9c68de9a6fca51e2a3dc1fe11069f5ed0
Jan-Paul Kleemans <jpkleemans@gmail.com>
2015-05-21 09:44:55 +0200

    Max recursion fix

--- src/scripts/controllers/TestController.js
+++ src/scripts/controllers/TestController.js
@@ -1,47 +1,51 @@
+honeydew.config(function ($rootScopeProvider) {
+    $rootScopeProvider.digestTtl(500);
+});
+
 honeydew.controller('TestController', function ($scope) {
+    var maxDepth = 499;
 
-    //var maxDepth = 5;
-    //
-    //var getChildren = function (depth) {
-    //    var result = [];
-    //
-    //    if (depth <= maxDepth) {
-    //        var array = new Array(5);
-    //
-    //        var i = array.length;
-    //        while (i--) {
-    //            result.push({
-    //                key: 'item' + i,
-    //                children: getChildren(depth + 1)
-    //            });
-    //        }
-    //    }
-    //
-    //    return result;
-    //};
-
-    var getItems = function () {
+    var getChildren = function (depth) {
         var result = [];
 
-        var array = new Array(60);
+        if (depth <= maxDepth) {
+            var array = new Array(1);
 
-        var i = array.length;
-        while (i--) {
-            var columns = new Array(16);
-            var cols = [];
-
-            var j = columns.length;
-            while (j--) {
-                cols.push('col' + j);
+            var i = array.length;
+            while (i--) {
+                result.push({
+                    key: 'item' + i,
+                    children: getChildren(depth + 1)
+                });
             }
-
-            result.push(cols);
         }
 
         return result;
     };
 
-    $scope.items = getItems();
+    //var getItems = function () {
+    //    var result = [];
+    //
+    //    var array = new Array(60);
+    //
+    //    var i = array.length;
+    //    while (i--) {
+    //        var columns = new Array(16);
+    //        var cols = [];
+    //
+    //        var j = columns.length;
+    //        while (j--) {
+    //            cols.push('col' + j);
+    //        }
+    //
+    //        result.push(cols);
+    //    }
+    //
+    //    return result;
+    //};
+
+    $scope.items = getChildren(0);
+    console.log($scope.items);
 
     $scope.add = function () {
         $scope.items.push('test');
